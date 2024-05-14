f3e5f4695f8913dd3a347f943016cfeffe6c7c9a
LeeHwangChoong <lyd8224@gmail.com>
2024-05-14 11:45:51 +0900

    init

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+    public Text playerNameText;
+
+    void Start()
+    {
+        // PlayerPrefs를 사용하여 저장된 이름 불러오기
+        string playerName = PlayerPrefs.GetString("PlayerName", "DefaultName");
+
+        // 가져온 이름을 원하는 텍스트 UI에 표시
+        playerNameText.text = "Player Name: " + playerName;
+    }
+}
+
